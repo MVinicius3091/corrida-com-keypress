9e2f036c1c6daa880f9ff104d47b413ae4b0fdc3
maykon vinicius <mvs_tj14@hotmail.com>
2021-12-07 21:39:10 -0300

    corrida de carrinhos

---
+++ function.js
@@ -0,0 +1,58 @@
+var car1 = document.getElementById('carro1');
+var car2 = document.getElementById('carro2');
+var fundo = document.getElementsByClassName('inicio')[0];
+var body = document.getElementById('body');
+
+var botao = document.getElementById('botao').addEventListener('click', function(){fundo.style.display = 'none';});
+
+var restarte = document.getElementById('novo').addEventListener('click', function(){novo()});
+
+
+
+
+var primeiro = 0;
+var segundo = 0;
+var velocidade = 30;
+
+body.addEventListener('keydown', function(e){ 
+
+  let correr = e.key;
+
+  if(correr == 'z'){
+    primeiro += velocidade;
+    car1.style.marginLeft = primeiro + 'px';   
+    vitoria(primeiro, 'Jogador 1');
+    console.log(primeiro);
+  }
+
+  if(correr == 'm'){
+    segundo += velocidade;
+    car2.style.marginLeft = segundo + 'px';
+    vitoria(segundo, 'Jogador 2');
+  }
+});
+
+
+
+
+function vitoria(final, vencedor){
+
+  if(final > 1180){
+   alert('O vencedor foi o ' + vencedor);
+  }
+
+}
+
+function novo(){
+
+  primeiro = 0;
+  segundo = 0;
+ 
+  car2.style.marginLeft = segundo + 'px';
+  car1.style.marginLeft = primeiro + 'px';  
+} 
+
+  
+
+
+
